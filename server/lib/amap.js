0ca1e0f2e97399db5d90d1d4d51eca0c8df43570
chenyuzhao <1209031611@qq.com>
2019-03-25 16:26:14 +0800

    修改格式

--- server/lib/amap.js
+++ server/lib/amap.js
@@ -1,44 +1,44 @@
-'use strict'
+'use strict';
 
 class Amap {
-    constructor(app){
-        this.app = app;
+  constructor(app) {
+    this.app = app;
 
-        this.gaodeKey = app.config.gaodeKey;
-        this.amapApiHost = app.config.amapApiHost;
-        this.weatherInfoURL = app.config.weatherInfoURL;
-        this.polygonURL = app.config.polygonURL;
-        this.districtURL = app.config.districtURL;
-    }
+    this.gaodeKey = app.config.gaodeKey;
+    this.amapApiHost = app.config.amapApiHost;
+    this.weatherInfoURL = app.config.weatherInfoURL;
+    this.polygonURL = app.config.polygonURL;
+    this.districtURL = app.config.districtURL;
+  }
 
-    async getWeatherInfo(place){
-        const url = `${this.amapApiHost}${this.weatherInfoURL}key=${this.gaodeKey}&city=${place}`;
-        this.app.logger.info(url);
-        let res = await this.app.curl(url,{
-            method:'GET',
-            dataType : 'json',
-            gzip: true,
-        });
-        let data = res.data;
-        if(data["status"] != "1"){
-            this.app.logger.info(`【获取天气信息失败】${data["info"]}`);
-        }
-        return data["lives"]
+  async getWeatherInfo(place) {
+    const url = `${this.amapApiHost}${this.weatherInfoURL}key=${this.gaodeKey}&city=${place}`;
+    this.app.logger.info(url);
+    const res = await this.app.curl(url, {
+      method: 'GET',
+      dataType: 'json',
+      gzip: true,
+    });
+    const data = res.data;
+    if (data.status !== '1') {
+      this.app.logger.info(`【获取天气信息失败】${data.info}`);
     }
-    async getDistrict(keywords){
-        const url = `${this.amapApiHost}${this.districtURL}key=${this.gaodeKey}&keywords=${keywords}&subdistrict=1&extensions=base`;
-        this.app.logger.info(url);
-        let res = await this.app.curl(url,{
-            method:'GET',
-            dataType : 'json',
-            gzip: true,
-        });
-        let data = res.data;
-        if(data["status"] != "1"){
-            this.app.logger.info(`【获取城市信息失败】${data["info"]}`);
-        }
-        return data["districts"]
+    return data.lives;
+  }
+  async getDistrict(keywords) {
+    const url = `${this.amapApiHost}${this.districtURL}key=${this.gaodeKey}&keywords=${keywords}&subdistrict=1&extensions=base`;
+    this.app.logger.info(url);
+    const res = await this.app.curl(url, {
+      method: 'GET',
+      dataType: 'json',
+      gzip: true,
+    });
+    const data = res.data;
+    if (data.status !== '1') {
+      this.app.logger.info(`【获取城市信息失败】${data.info}`);
     }
+    return data.districts;
+  }
 }
 
 module.exports = Amap;
